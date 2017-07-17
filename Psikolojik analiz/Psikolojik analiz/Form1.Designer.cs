bdde759e8d1db6492ffe07268ee88b0cec96878c
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 19:28:20 +0300

    Add files via upload

---
+++ Psikolojik analiz/Psikolojik analiz/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(382, 195);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(0, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(382, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(0, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(382, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(0, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(382, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(0, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(382, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(0, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(382, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "E";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(417, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 55);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Başla";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(397, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Toplam Puanınız:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(522, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(400, 166);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(148, 39);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Sonucu Gör";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(400, 236);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(148, 39);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Yeniden Dene";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(560, 413);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Psikolojik Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Psikolojik analiz/Psikolojik analiz/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void btntrue() {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            
+        }
+        public void btnfalse() {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+            
+
+        }
+        int soru = 0;
+        int toplam = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            toplam += 1;
+            btnfalse();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button7.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            button8.Visible = false;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(soru <= 12){
+                soru++;
+
+            }
+          
+            button6.Text = "Sonraki Soru";
+            btntrue();
+            button6.Enabled = false;
+            if (soru == 1)
+            {
+                textBox1.Text = "1-Sevgilinizle ya da eşinizle “mükemmel bir akşam” size aşağıdakilerden hangisini çağrıştırıyor?";
+                button1.Text = "Bir rock konserine gitmek";
+                button2.Text = "Lunaparka gitmek";
+                button3.Text = "Sinemaya gitmek ";
+                button4.Text = "Mum ışığında romantik bir yemek";
+                button5.Text = "Arabayla turlamak";
+
+            }
+            if (soru == 2)
+            {
+                textBox1.Text = "2) En sevdiğiniz müzik türü hangisi?";
+                button1.Text = "Alternatif";
+                button2.Text = "Rock And Roll";
+                button3.Text = "Pop";
+                button4.Text = "Soft Rock";
+                button5.Text = "RAP!";
+            }
+            if (soru == 3)
+            {
+                textBox1.Text = "3)En çok hangi tür filmleri seversiniz?";
+                button1.Text = "Korku";
+                button2.Text = "Komedi";
+                button3.Text = "Müzikal";
+                button4.Text = "Romantik ";
+                button5.Text = "Belgesel";
+            }
+            if (soru == 4)
+            {
+                textBox1.Text = "4) Aşağıdaki mesleklerden birini seçmeniz gerekseydi hangisi olurdunuz?";
+                button1.Text = "Barmen/Barmaid";
+                button2.Text = "Polis";
+                button3.Text = "Öğretmen";
+                button4.Text = "Garson ";
+                button5.Text = "Futbolcu";
+            }
+            if (soru == 5)
+            {
+                textBox1.Text = "5) Bir saat boş vaktinizi değerlendirmek için hangisi size daha uygun?";
+                button1.Text = "TV izlemek ";
+                button2.Text = "İnternete Girmek";
+                button3.Text = "Uyumak";
+                button4.Text = "Okumak ";
+                button5.Text = "Dışarıda Çalışmak";
+            }
+            if (soru == 6)
+            {
+                textBox1.Text = "6) Aşağıdaki renklerden birini seçiniz.";
+                button1.Text = "Sarı";
+                button2.Text = "Kahverengi";
+                button3.Text = "Mavi";
+                button4.Text = "Kırmızı ";
+                button5.Text = "Beyaz";
+            }
+            if (soru == 7)
+            {
+                textBox1.Text = "7) Şu an önünüzde aşağıdaki yiyeceklerden hangisi olsa dayanamayıp götürürsünüz?";
+                button1.Text = "Suşi";
+                button2.Text = "Pizza";
+                button3.Text = "Dondurma";
+                button4.Text = "Makarna ";
+                button5.Text = "Salata";
+            }
+            if (soru == 8)
+            {
+                textBox1.Text = "8 ) En sevdiğiniz tatil, bayram ya da özel gün hangisi?";
+                button1.Text = " 23 Nisan Çocuk Bayramı ";
+                button2.Text = "Yılbaşı Tatili ";
+                button3.Text = "Ramazan Bayramı";
+                button4.Text = "Sevgililer Günü";
+                button5.Text = "Kurban Bayramı";
+            }
+            if (soru == 9)
+            {
+                textBox1.Text = "9) Şu an aşağıdakilerden hangisinde olmak isterdiniz?";
+                button1.Text = "Las Vegas’ta ";
+                button2.Text = "Hawaii’de";
+                button3.Text = "Hollywood’da";
+                button4.Text = "Paris’te";
+                button5.Text = "İspanya'da";
+            }
+            if (soru == 10)
+            {
+                textBox1.Text = "10) Nasıl biriyle vakit geçirmekten hoşlanırsınız?";
+                button1.Text = "Bir parti canavarıyla ";
+                button2.Text = "Güzel/Yakışıklı biriyle ";
+                button3.Text = " Komik biriyle ";
+                button4.Text = "Duygusal biriyle  ";
+                button5.Text = " Akıllı biriyle ";
+            }
+            if (soru == 11)
+            {
+                button7.Visible = true;
+                button6.Enabled = false;
+                label2.Text = toplam.ToString();
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                label1.Visible = true;
+                label2.Visible = true;
+                
+            }
+            
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //burdaki kod butonlar verdiğimiz değerlerin toplamında kaç çıkıyorsa ona göre yazı yazdırmak
+            if (toplam >= 10 && toplam <= 17)//toplam 10dan büyükeşit ve 17den küçük eşitse
+            {
+                button8.Visible = true;
+                textBox1.Text = "Siz vahşi ve çılgın birisiniz. Eğleneyim derken uçuyorsunuz. Ama her zaman ne yaptığınızın farkındasınız, hayatınızın kontrolünü kaybetmiyorsunuz. İnsanlar olaylara sizin gibi bakmadığında bu sizi durdurmuyor.Tavsiye: Bu vahşi karakterinizle kendinizi ve etrafınızdakileri incitebilirsiniz. Dikkatli olun.";
+               
+            }
+            button8.Visible = true;
+
+            if (toplam >= 18 && toplam <= 26)
+            {
+                button8.Visible = true;
+                textBox1.Text = "Siz eğlenceli, arkadaş canlısı ve popülersiniz. Cıvıl cıvıl halinizle etrafa neşe saçıyorsunuz. Çoğu zaman ikili ilişkilerinizde uçuk kaçık davransanız da inandığınız değerlerden asla taviz vermiyorsunuz. Evlilik ve çocuk sahibi olmak sizin için önemli ama öncelikle yeterince tecrübe edinmek istiyorsunuz.Tavsiye: İnsanların aldığınız kararlarda sizi bu kadar etkilemelerine izin vermeyin.";
+            }
+            if (toplam >= 27 && toplam <= 34)
+            {
+                button8.Visible = true;
+                textBox1.Text = "Öyle tatlısınız ki sizi sevmemek imkansız. Siz insanların asla kaybetmek istemeyeceği türden bir dostsunuz. Kimseyi incitmemek için kendinizi incittiğiniz bile oluyor. Siz genellikle olaylar karşısında olgun ve sakin bir tavır takınırsınız.Tavsiye: Herkes göründüğü gibi değildir, bunu unutmayın. Biraz kurnaz olun.";
+
+            }
+            if (toplam >= 35 && toplam <= 42)
+            {
+                button8.Visible = true;
+                textBox1.Text = "Siz iflah olmaz bir romantiksiniz. Şarap, kırmızı güller, bunlar sizin yaşam tarzınız. Söz verirsiniz ve o sözü ne olursa olsun yerine getirirsiniz. Ailenize çok bağlısınız. Annenizi her haftasonu ararsınız, özel günleri asla unutmazsınız.Tavsiye: Fazla romantizmin sizi gerçeklerden uzaklaştırmasına izin vermeyin.";
+            }
+            if (toplam >= 43 && toplam <= 50)
+            {
+                button8.Visible = true;
+                textBox1.Text = "Belirgin özelliğiniz hızlı ve doğru karar verebilmeniz. Her olay için kafanızda bir planınız vardır. Fiziksel ve zihinsel gücünüz çok fazla. Mantıklı, ılımlı ve yapıcı tavırlarınızla çoğu kişinin kendine örnek aldığı birisiniz.Tavsiye: Küçük gibi görünen bazı olayları gözardı etmeniz ileride büyük sorunlara yol açabilir.";
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            toplam += 2;
+            btnfalse();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            toplam += 3;
+            btnfalse();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            toplam += 4;
+            btnfalse();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            toplam += 5;
+            btnfalse();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (soru == 11)
+            {
+                toplam = 0;
+                soru = 0;
+                button7.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                button6.Enabled = true;
+                button8.Visible = false;
+
+            }
+        }
+    }
+}
